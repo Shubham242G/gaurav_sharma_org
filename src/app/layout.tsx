7a213f28574de48519e2e163e21cbcc9f4707450
Shubham242G <allhailvicky242@@gmail.com>
2025-11-24 14:05:35 +0530

    Message to whatsapp updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import FloatingNav from "./components/FloatingNav";
+import WhatsAppFloat from "./components/WhatsappFloat";
 
 export const metadata: Metadata = {
   title: "Gaurav Sharma Law Offices",
@@ -18,6 +19,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}>
         <FloatingNav />
+        <WhatsAppFloat 
+          phoneNumber="918595650338" 
+          message="Hello! I'd like to inquire about your legal services."
+        />
         {children}
       </body>
     </html>
